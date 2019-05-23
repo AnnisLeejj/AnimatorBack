9286268bf06b920f9bb405961ca5e763b591f5a3
DESKTOP-9B4UMLR\Annis <l536510961@126.com>
2019-05-23 18:00:58 +0800

    init

--- app/src/main/java/com/annislee/animatorback/widget/MyPlane.kt
+++ app/src/main/java/com/annislee/animatorback/widget/MyPlane.kt
@@ -22,12 +22,7 @@ class MyPlane : AppCompatImageView {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init()
     }
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
-            : super(context, attrs, defStyleAttr, defStyleRes) {
-        init()
-    }
-
+ 
     private fun init() {
 
     }
